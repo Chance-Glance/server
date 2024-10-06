82287fe6c6c827ec83759856ad11f4f38274708e
mungsil <thddmd2009@gmail.com>
2024-10-07 04:21:18 +0900

    feat: google near place API response DTO 작성 #17

--- src/main/java/com/example/mohago_nocar/place/infrastructure/externalApi/dto/GoogleNearByPlaceResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.mohago_nocar.place.infrastructure.externalApi.dto;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import java.util.Optional;
-
-public record GoogleNearByPlaceResponse(
-        Optional<JsonNode> results
-) {
-}
