6e3d44d008c521ab37f943d0025f627a881a95cb
mungsil <thddmd2009@gmail.com>
2024-10-07 05:21:56 +0900

    fix: 장소 타입 매핑 오류 수정 #17

--- src/main/java/com/example/mohago_nocar/place/domain/model/PlaceType.java
+++ src/main/java/com/example/mohago_nocar/place/domain/model/PlaceType.java
@@ -4,37 +4,16 @@ public enum PlaceType {
     RESTAURANT, ATTRACTION;
 
     public static PlaceType from(String placeType) {
-        return switch (placeType.toLowerCase()) {
-            case
-                    "restaurant",
-                    "barbecue_restaurant",
-                    "bakery",
-                    "breakfast_restaurant",
-                    "brunch_restaurant",
-                    "cafe",
-                    "chinese_restaurant",
-                    "coffee_shop",
-                    "fast_food_restaurant",
-                    "french_restaurant",
-                    "hamburger_restaurant",
-                    "ice_cream_shop",
-                    "indian_restaurant",
-                    "indonesian_restaurant",
-                    "italian_restaurant",
-                    "japanese_restaurant",
-                    "korean_restaurant",
-                    "mediterranean_restaurant",
-                    "mexican_restaurant",
-                    "middle_eastern_restaurant",
-                    "pizza_restaurant",
-                    "ramen_restaurant",
-                    "sandwich_shop",
-                    "seafood_restaurant",
-                    "steak_house",
-                    "sushi_restaurant"
-                    -> RESTAURANT;
+        if (placeType.contains("음식점")
+                || placeType.contains("식당")
+                || placeType.contains("카페")
+                || placeType.contains("커피숍/커피 전문점")
+                || placeType.contains("제과점")
+                || placeType.contains("아이스크림 가게")
+        ) {
+            return RESTAURANT;
+        }
 
-            default -> ATTRACTION;
-        };
+        return ATTRACTION;
     }
 }
