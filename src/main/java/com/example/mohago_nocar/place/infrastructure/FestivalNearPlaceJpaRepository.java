b6913a1964bf8d5f9cca31d30d59591c0a67fbd1
mungsil <thddmd2009@gmail.com>
2024-10-07 04:16:39 +0900

    feat: festival near place의 repository 작성 #17

---
+++ src/main/java/com/example/mohago_nocar/place/infrastructure/FestivalNearPlaceJpaRepository.java
@@ -0,0 +1,8 @@
+package com.example.mohago_nocar.place.infrastructure;
+
+import com.example.mohago_nocar.place.domain.model.FestivalNearPlace;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FestivalNearPlaceJpaRepository extends JpaRepository<FestivalNearPlace, Long> {
+
+}
