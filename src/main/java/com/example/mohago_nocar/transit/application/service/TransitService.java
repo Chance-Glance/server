e4dfd4b40fd568ed5545fbae21f4a541ec562b8e
mungsil <thddmd2009@gmail.com>
2024-10-07 22:44:23 +0900

    feat: 도메인 객체 변환을 담당하는 새로운 mapper 작성 #20

--- src/main/java/com/example/mohago_nocar/transit/application/service/TransitService.java
+++ src/main/java/com/example/mohago_nocar/transit/application/service/TransitService.java
@@ -1,10 +1,11 @@
 package com.example.mohago_nocar.transit.application.service;
 
+import com.example.mohago_nocar.global.common.domain.vo.Location;
+import com.example.mohago_nocar.transit.application.mapper.TransitMapper;
 import com.example.mohago_nocar.transit.domain.model.TransitInfo;
 import com.example.mohago_nocar.transit.domain.service.TransitUseCase;
 import com.example.mohago_nocar.transit.infrastructure.externalApi.ODsayApiClient;
-import com.example.mohago_nocar.transit.infrastructure.externalApi.dto.OdsayResponse;
-import com.example.mohago_nocar.transit.infrastructure.mapper.TransitPathMapper;
+import com.example.mohago_nocar.transit.infrastructure.externalApi.dto.response.RouteResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,6 @@ public class TransitService implements TransitUseCase {
     public TransitInfo findTransitInfo(double startX, double startY, double endX, double endY) {
         OdsayResponse response = oDsayApiClient.request(startX, startY, endX, endY);
 
-        return TransitPathMapper.mapToTransitInfo(response);
+        return TransitMapper.mapRouteResponseDtoToTransitInfo(response);
     }
 }
