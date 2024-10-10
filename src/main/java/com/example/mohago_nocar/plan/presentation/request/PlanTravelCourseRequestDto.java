88f3f1375c9b360996f7e51d4eed28cf8354f68f
mungsil <thddmd2009@gmail.com>
2024-10-10 23:45:31 +0900

    refactor: request DTO 필드에 대한 스웨거 설명 수정 #25

--- src/main/java/com/example/mohago_nocar/plan/presentation/request/PlanTravelCourseRequestDto.java
+++ src/main/java/com/example/mohago_nocar/plan/presentation/request/PlanTravelCourseRequestDto.java
@@ -16,7 +16,7 @@ public record PlanTravelCourseRequestDto(
         @Schema(description = "도착 시간", example = "10:00")
         LocalTime arrivalTime,
 
-        @Schema(description = "googlePlaceId 리스트", example = "[\"ChIJZ4iA7KrebjURMcTOW3a5dJ0\"]")
+        @Schema(description = "선택된 여행 장소들의 GooglePlaceId", example = "[\"ChIJZ4iA7KrebjURMcTOW3a5dJ0\"]")
         List<String> travelPlaceIds
 ) {
 }
