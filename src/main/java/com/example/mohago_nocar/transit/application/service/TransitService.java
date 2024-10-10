ce99987cdd7b28eb8d8c82283b18408d71a497e2
Kim Songeun <thddmd2009@gmail.com>
2024-10-10 14:01:08 +0900

    길찾기(ODsay) API 에러 응답 바인딩 및 리팩토링 완료 (#21)
    * refactor: Location from 메서드 파라미터를 x,y 순서대로 변경 #20
    
    * feat: 커스텀 Exception 작성 및 ExceptionHandler 수정 #20
    
    * feat: ODsay 커스텀 Exception 작성 #20
    
    * refactor: 역할이 드러나는 이름으로 ODsay 응답 DTO명 변경 #20
    
    * fix: ODsay 에러 응답 바인딩 문제 해결, mapper 책임 분리 #20
    
    * feat: 도메인 객체 변환을 담당하는 새로운 mapper 작성 #20
    
    * fix: 커스텀 역직렬화 클래스를 사용하는 RestClient 인스턴스 주입 #20
    
    * fix: 변경된 ODsay API 응답 DTO 적용과 메서드 가독성 개선 #20
    
    * refactor: 서비스 메서드 가독성 개선 #20
    
    * refactor: 커스텀 예외 적용 #20
    
    * refactor: 커스텀 예외 적용 #20
    
    * refactor: 로거를 이용한 로그 출력 #20
    
    * refactor: 메서드 파라미터 이름 통일 #20

--- src/main/java/com/example/mohago_nocar/transit/application/service/TransitService.java
+++ src/main/java/com/example/mohago_nocar/transit/application/service/TransitService.java
@@ -1,10 +1,11 @@
 package com.example.mohago_nocar.transit.application.service;
 
+import com.example.mohago_nocar.global.common.domain.vo.Location;
+import com.example.mohago_nocar.transit.application.mapper.TransitMapper;
 import com.example.mohago_nocar.transit.domain.model.TransitInfo;
 import com.example.mohago_nocar.transit.domain.service.TransitUseCase;
 import com.example.mohago_nocar.transit.infrastructure.externalApi.ODsayApiClient;
-import com.example.mohago_nocar.transit.infrastructure.externalApi.dto.OdsayResponse;
-import com.example.mohago_nocar.transit.infrastructure.mapper.TransitPathMapper;
+import com.example.mohago_nocar.transit.infrastructure.externalApi.dto.response.RouteResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,9 +16,14 @@ public class TransitService implements TransitUseCase {
     private final ODsayApiClient oDsayApiClient;
 
     @Override
-    public TransitInfo findTransitInfo(double startX, double startY, double endX, double endY) {
-        OdsayResponse response = oDsayApiClient.request(startX, startY, endX, endY);
+    public TransitInfo findRouteTransitBetweenPlaces(Location from, Location to) {
+        RouteResponseDto response = oDsayApiClient.searchRoute(
+                from.getLongitude(),
+                from.getLatitude(),
+                to.getLongitude(),
+                to.getLatitude()
+        );
 
-        return TransitPathMapper.mapToTransitInfo(response);
+        return TransitMapper.mapRouteResponseDtoToTransitInfo(response);
     }
 }
