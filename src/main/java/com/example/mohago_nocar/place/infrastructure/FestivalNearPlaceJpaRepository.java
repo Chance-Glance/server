486541715e88d01acfb156ff7bea3268e3ca8ebc
mingmingmon <96719969+mingmingmon@users.noreply.github.com>
2024-10-11 14:34:55 +0900

    feat: 축제 주변 장소 조회 API 작성 완료 #22 (#24)
    * feat: 축제 주변 장소 조회 API 작성 완료 #22
    
    * fix: 잘못된 타입 변환 수정, stream.map 함수 추출 #22

--- src/main/java/com/example/mohago_nocar/place/infrastructure/FestivalNearPlaceJpaRepository.java
+++ src/main/java/com/example/mohago_nocar/place/infrastructure/FestivalNearPlaceJpaRepository.java
@@ -1,8 +1,12 @@
 package com.example.mohago_nocar.place.infrastructure;
 
 import com.example.mohago_nocar.place.domain.model.FestivalNearPlace;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FestivalNearPlaceJpaRepository extends JpaRepository<FestivalNearPlace, Long> {
+
+    Page<FestivalNearPlace> findAllByFestivalId(Long festivalId, Pageable pageable);
 
 }
