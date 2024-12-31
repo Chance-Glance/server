5fb33723ac5ea2e3043874dae2a71382ecd98042
mungsil <thddmd2009@gmail.com>
2025-01-01 00:13:28 +0900

    feat: transitRoute useCase 작성 #31

--- src/main/java/com/example/mohago_nocar/transit/domain/service/TransitUseCase.java
+++ src/main/java/com/example/mohago_nocar/transit/domain/service/TransitUseCase.java
@@ -1,9 +1,17 @@
 package com.example.mohago_nocar.transit.domain.service;
 
 import com.example.mohago_nocar.global.common.domain.vo.Location;
-import com.example.mohago_nocar.transit.domain.model.TransitInfo;
+import com.example.mohago_nocar.transit.domain.model.RoutePoint;
+import com.example.mohago_nocar.transit.domain.model.TransitRoute;
+import com.example.mohago_nocar.transit.domain.model.TransitRouteWithSegments;
+import com.example.mohago_nocar.transit.infrastructure.odsay.dto.response.ODsayApiResponse;
 
 public interface TransitUseCase {
 
-    TransitInfo findRouteTransitBetweenPlaces(Location from, Location to);
+    TransitRouteWithSegments findTransitRouteWithSegments(Location from, Location to);
+
+    TransitRoute findTransitRoute(Location from, Location to);
+
+    void saveTransitRouteWithSegments(ODsayApiResponse apiResponse, RoutePoint departure, RoutePoint arrival);
+
 }
