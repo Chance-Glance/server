c7761026bb79891add19623416bd5c2b3e77b5a0
mungsil <thddmd2009@gmail.com>
2025-01-08 21:04:24 +0900

    fix: TransitUseCase 메서드 매개변수 타입 및 개수 변경 #31

--- src/main/java/com/example/mohago_nocar/transit/domain/service/TransitUseCase.java
+++ src/main/java/com/example/mohago_nocar/transit/domain/service/TransitUseCase.java
@@ -1,10 +1,9 @@
 package com.example.mohago_nocar.transit.domain.service;
 
 import com.example.mohago_nocar.global.common.domain.vo.Location;
-import com.example.mohago_nocar.transit.domain.model.RoutePoint;
 import com.example.mohago_nocar.transit.domain.model.TransitRoute;
 import com.example.mohago_nocar.transit.domain.model.TransitRouteWithSegments;
-import com.example.mohago_nocar.transit.infrastructure.odsay.dto.response.ODsayApiResponse;
+import com.example.mohago_nocar.transit.infrastructure.odsay.dto.response.ODsayApiSuccessResponseWrapper;
 
 public interface TransitUseCase {
 
@@ -12,6 +11,6 @@ public interface TransitUseCase {
 
     TransitRoute findTransitRoute(Location from, Location to);
 
-    void saveTransitRouteWithSegments(ODsayApiResponse apiResponse, RoutePoint departure, RoutePoint arrival);
+    void saveTransitRouteWithSegments(ODsayApiSuccessResponseWrapper response);
 
 }
