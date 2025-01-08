6969dc6f9527b469f1aad2de3958fef66895486f
mungsil <thddmd2009@gmail.com>
2025-01-08 16:32:43 +0900

    refactor: 가독성 개선 #31

--- src/main/java/com/example/mohago_nocar/transit/infrastructure/batch/config/FreshMessageTasklet.java
+++ src/main/java/com/example/mohago_nocar/transit/infrastructure/batch/config/FreshMessageTasklet.java
@@ -4,6 +4,7 @@ import com.example.mohago_nocar.transit.infrastructure.batch.persistence.OdsayAp
 import com.example.mohago_nocar.transit.infrastructure.batch.persistence.OdsayApiRequestEntryRepository;
 import com.example.mohago_nocar.transit.infrastructure.messaging.persistence.OdsayApiRequestEvent;
 import com.example.mohago_nocar.transit.infrastructure.messaging.TransitRouteEventManager;
+import com.example.mohago_nocar.transit.infrastructure.odsay.client.ODsayApiLimitProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
@@ -18,41 +19,67 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static com.example.mohago_nocar.transit.infrastructure.odsay.client.ODsayApiLimitProvider.DEFAULT_BATCH_LIMIT_PER_DAY;
-
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class FreshMessageTasklet implements Tasklet {
 
+    private static final String PROCESSED_FRESH_MESSAGES = "processedFreshMessages";
+    private static final String PROCESSED_DEFERRED_MESSAGES = "processedDeferredMessages";
+
     private final OdsayApiRequestEntryRepository odsayApiRequestEntryRepository;
     private final TransitRouteEventManager messagingService;
+    private final ODsayApiLimitProvider oDsayApiLimitProvider;
 
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-        ExecutionContext executionContext = contribution.getStepExecution().getJobExecution().getExecutionContext();
-
-        int attempted = executionContext.getInt("processedDeferredMessages");
-        int remained = DEFAULT_BATCH_LIMIT_PER_DAY - attempted;
-        log.info(">>> remained {} messages ", remained);
-
+        ExecutionContext executionContext = getExecutionContext(contribution);
+        int availableCount = calcTransmittableCount(executionContext);
         int processedFreshMessageCount = 0;
-        if (remained > 0) {
-            PageRequest pageRequest = PageRequest.of(0, remained, Sort.by(Sort.Direction.ASC, "createdAt"));
-            List<OdsayApiRequestEntry> freshMessages = odsayApiRequestEntryRepository.findAllByOrderByCreatedAtAsc(pageRequest);
 
-            for (OdsayApiRequestEntry freshMessage : freshMessages) {
-                messagingService.sendToPriorityQueue(OdsayApiRequestEvent.parse(freshMessage));
-            }
-
+        if (has(availableCount)) {
+            List<OdsayApiRequestEntry> freshMessages = findApiRequestEntries(availableCount);
+            sendMessageToQueue(freshMessages);
+            cleanSavedMessages(freshMessages);
             processedFreshMessageCount = freshMessages.size();
-            odsayApiRequestEntryRepository.deleteAllByIdInBatch(freshMessages.stream()
-                    .map(OdsayApiRequestEntry::getId).collect(Collectors.toList()));
         }
 
-        executionContext.putInt("processedFreshMessages", processedFreshMessageCount);
+        executionContext.putInt(PROCESSED_FRESH_MESSAGES, processedFreshMessageCount);
 
         return RepeatStatus.FINISHED;
+    }
+
+    private int calcTransmittableCount(ExecutionContext executionContext) {
+        int processedCount = getProcessedDeferredMessages(executionContext);
+        return oDsayApiLimitProvider.getDailyBatchLimit() - processedCount;
+    }
+
+    private List<OdsayApiRequestEntry> findApiRequestEntries(int availableCount) {
+        PageRequest pageRequest = PageRequest.of(0, availableCount, Sort.by(Sort.Direction.ASC, "createdAt"));
+        return odsayApiRequestEntryRepository.findAllByOrderByCreatedAtAsc(pageRequest);
+    }
+
+    private void sendMessageToQueue(List<OdsayApiRequestEntry> freshMessages) {
+        for (OdsayApiRequestEntry freshMessage : freshMessages) {
+            messagingService.sendToPriorityQueue(OdsayApiRequestEvent.parse(freshMessage));
+        }
+    }
+
+    private void cleanSavedMessages(List<OdsayApiRequestEntry> freshMessages) {
+        odsayApiRequestEntryRepository.deleteAllByIdInBatch(freshMessages.stream()
+                .map(OdsayApiRequestEntry::getId).collect(Collectors.toList()));
+    }
+
+    private boolean has(int remainingCount) {
+        return remainingCount > 0;
+    }
+
+    private int getProcessedDeferredMessages(ExecutionContext executionContext) {
+        return executionContext.getInt(PROCESSED_DEFERRED_MESSAGES);
+    }
+
+    private ExecutionContext getExecutionContext(StepContribution contribution) {
+        return contribution.getStepExecution().getJobExecution().getExecutionContext();
     }
 
 }
