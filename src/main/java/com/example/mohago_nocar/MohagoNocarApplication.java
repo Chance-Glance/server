3d2d93c8df2e7602815d655887495ec9e9e3eff7
mungsil <thddmd2009@gmail.com>
2025-01-08 22:28:02 +0900

    feat: @EnableBatchProcessing 작성 #31

--- src/main/java/com/example/mohago_nocar/MohagoNocarApplication.java
+++ src/main/java/com/example/mohago_nocar/MohagoNocarApplication.java
@@ -1,9 +1,11 @@
 package com.example.mohago_nocar;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class MohagoNocarApplication {
 
 	public static void main(String[] args) {
