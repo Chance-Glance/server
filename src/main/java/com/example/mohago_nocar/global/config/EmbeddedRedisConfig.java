773e783d489055d39f66944b09f09ce78e0e32a6
mungsil <thddmd2009@gmail.com>
2025-01-26 16:34:44 +0900

    feat: 테스트용 redis 설정 #34

---
+++ src/main/java/com/example/mohago_nocar/global/config/EmbeddedRedisConfig.java
@@ -0,0 +1,32 @@
+package com.example.mohago_nocar.global.config;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import redis.embedded.RedisServer;
+
+@Configuration
+@Profile("test")
+public class EmbeddedRedisConfig {
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void startEmbeddedRedis() {
+        redisServer = new RedisServer(port);
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopEmbeddedRedis() {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+
+}
